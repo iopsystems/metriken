cc8531f35eb5e3f373469a38972674c639cc4559
Brian Martin <brayniac@gmail.com>
2024-02-16 15:06:47 -0800

    replace kind filters with a filter function

--- metriken-exposition/src/snapshotter.rs
+++ metriken-exposition/src/snapshotter.rs
@@ -1,12 +1,10 @@
+use std::any::Any;
 use crate::*;
 
 /// Produces a snapshot of metric readings.
 pub struct Snapshotter {
-    counters: bool,
-    gauges: bool,
-    atomic_histograms: bool,
-    rwlock_histograms: bool,
-    filter: fn(&str) -> bool,
+    kind_filter: fn(Option<&dyn Any>) -> bool,
+    name_filter: fn(&str) -> bool,
 }
 
 /// Used to build a new `Snapshotter`.
@@ -27,35 +25,19 @@ impl SnapshotterBuilder {
         self.snapshotter
     }
 
-    /// Allow disabling the inclusion of counter metrics from the snapshot.
-    pub fn counters(mut self, included: bool) -> Self {
-        self.snapshotter.counters = included;
-        self
-    }
-
-    /// Allow disabling the inclusion of gauge metrics from the snapshot.
-    pub fn gauges(mut self, included: bool) -> Self {
-        self.snapshotter.gauges = included;
-        self
-    }
-
-    /// Allow disabling the inclusion of `AtomicHistogram`s from the snapshot.
-    pub fn atomic_histograms(mut self, included: bool) -> Self {
-        self.snapshotter.atomic_histograms = included;
-        self
-    }
-
-    /// Allow disabling the inclusion of `RwLockHistogram`s from the snapshot.
-    pub fn rwlock_histograms(mut self, included: bool) -> Self {
-        self.snapshotter.rwlock_histograms = included;
+    /// Allow a user-supplied filtering function to be applied based on the
+    /// metric name. The function is given the metric name and must return true
+    /// for any metric that should be included in the snapshot.
+    pub fn name_filter(mut self, filter: fn(&str) -> bool) -> Self {
+        self.snapshotter.name_filter = filter;
         self
     }
 
     /// Allow a user-supplied filtering function to be applied. The function is
     /// given the metric name and must return true for any metric that should
     /// be included in the snapshot.
-    pub fn filter(mut self, filter: fn(&str) -> bool) -> Self {
-        self.snapshotter.filter = filter;
+    pub fn kind_filter(mut self, filter: fn(Option<&dyn std::any::Any>) -> bool) -> Self {
+        self.snapshotter.kind_filter = filter;
         self
     }
 }
@@ -63,11 +45,8 @@ impl SnapshotterBuilder {
 impl Default for Snapshotter {
     fn default() -> Self {
         Self {
-            counters: true,
-            gauges: true,
-            atomic_histograms: true,
-            rwlock_histograms: true,
-            filter: |_| true,
+            kind_filter: |_| true,
+            name_filter: |_| true,
         }
     }
 }
@@ -79,45 +58,33 @@ impl Snapshotter {
 
         // iterate through the metrics and build-up the snapshot
         for metric in &metriken::metrics() {
-            if !(self.filter)(metric.name()) {
+            if !(self.name_filter)(metric.name()) {
+                continue;
+            }
+
+            if ! (self.kind_filter)(metric.as_any()) {
                 continue;
             }
 
             match metric.value() {
                 Some(Value::Counter(value)) => {
-                    if !self.counters {
-                        continue;
-                    }
-
                     snapshot
                         .counters
                         .push((metric.formatted(metriken::Format::Simple), value));
                 }
                 Some(Value::Gauge(value)) => {
-                    if !self.gauges {
-                        continue;
-                    }
-
                     snapshot
                         .gauges
                         .push((metric.formatted(metriken::Format::Simple), value));
                 }
                 Some(Value::Other(other)) => {
                     if let Some(histogram) = other.downcast_ref::<AtomicHistogram>() {
-                        if !self.atomic_histograms {
-                            continue;
-                        }
-
                         if let Some(histogram) = histogram.snapshot() {
                             snapshot
                                 .histograms
                                 .push((metric.formatted(metriken::Format::Simple), histogram));
                         }
                     } else if let Some(histogram) = other.downcast_ref::<RwLockHistogram>() {
-                        if !self.rwlock_histograms {
-                            continue;
-                        }
-
                         if let Some(histogram) = histogram.snapshot() {
                             snapshot
                                 .histograms
