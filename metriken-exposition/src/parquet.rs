d794a3d7f4d6d31e77a0e052b96f25cd7fbe2424
Mihir Nanavati <code@mihirnanavati.com>
2025-02-19 23:27:31 -0500

    exposition: derive metric name from metadata for rezolus v5 snapshots

--- metriken-exposition/src/parquet.rs
+++ metriken-exposition/src/parquet.rs
@@ -165,19 +165,18 @@ impl ParquetSchema {
             (snapshot.counters, snapshot.gauges, snapshot.histograms);
 
         for counter in counters {
-            self.counters
-                .entry(counter.name)
-                .or_insert(counter.metadata);
+            let name = Snapshot::derive_metric_name(&counter.name, &counter.metadata);
+            self.counters.entry(name).or_insert(counter.metadata);
         }
 
         for gauge in gauges {
-            self.gauges.entry(gauge.name).or_insert(gauge.metadata);
+            let name = Snapshot::derive_metric_name(&gauge.name, &gauge.metadata);
+            self.gauges.entry(name).or_insert(gauge.metadata);
         }
 
         for histogram in histograms {
-            self.histograms
-                .entry(histogram.name)
-                .or_insert(histogram.metadata);
+            let name = Snapshot::derive_metric_name(&histogram.name, &histogram.metadata);
+            self.histograms.entry(name).or_insert(histogram.metadata);
         }
 
         if self.metadata.is_empty() && !snapshot.metadata.is_empty() {
--- metriken-exposition/src/snapshot.rs
+++ metriken-exposition/src/snapshot.rs
@@ -1,13 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::time::SystemTime;
 
 #[cfg(feature = "msgpack")]
 use rmp_serde::encode::Error as SerializeMsgpackError;
 #[cfg(feature = "json")]
 use serde_json::Error as JsonError;
-
-// TODO(bmartin): derive Debug for Snapshot once the histogram snapshot has its
-// own debug impl.
 
 #[derive(Clone, Debug)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -66,6 +63,52 @@ impl Snapshot {
         }
     }
 
+    /// Return the metric name: for Rezolus v4 data, this is the metric name
+    /// from the snapshot. Rezolus v5 snapshots have metrics with opaque names
+    /// with the real name being in the metadata.
+    pub(crate) fn derive_metric_name(
+        snapshot_name: &str,
+        metadata: &HashMap<String, String>,
+    ) -> String {
+        // Check for Rezolus v4 snapshot, if so return the name as-is
+        if snapshot_name.contains("/")
+            && snapshot_name
+                .chars()
+                .next()
+                .map(|x| x.is_ascii_digit())
+                .is_some_and(|x| !x)
+        {
+            return snapshot_name.to_string();
+        }
+
+        // Rezolus v5 snapshot: append all the metadata, except few known keys,
+        // to the name to ensure a unique name.
+        let ordered = ["name", "op", "state"];
+        let mut ignore: HashSet<&str> =
+            ["metric", "unit", "grouping_power", "max_value_power"].into();
+        ignore.extend(ordered);
+
+        let Some(name) = metadata.get("metric") else {
+            return snapshot_name.to_string();
+        };
+        let mut unique_name = name.to_string();
+
+        for k in ordered {
+            if let Some(v) = metadata.get(k) {
+                unique_name = unique_name + "/" + v;
+            }
+        }
+
+        for (k, v) in metadata {
+            if ignore.contains(k.as_str()) {
+                continue;
+            }
+            unique_name = unique_name + "/" + v;
+        }
+
+        unique_name
+    }
+
     /// The system time when the snapshot was created.
     pub fn systemtime(&self) -> SystemTime {
         self.systemtime
@@ -119,12 +162,24 @@ impl From<Snapshot> for HashedSnapshot {
             .expect("System Clock is earlier than 1970; needs reset")
             .as_nanos() as u64;
 
-        let counters: HashMap<String, Counter> =
-            HashMap::from_iter(snapshot.counters.into_iter().map(|v| (v.name.clone(), v)));
-        let gauges: HashMap<String, Gauge> =
-            HashMap::from_iter(snapshot.gauges.into_iter().map(|v| (v.name.clone(), v)));
-        let histograms: HashMap<String, Histogram> =
-            HashMap::from_iter(snapshot.histograms.into_iter().map(|v| (v.name.clone(), v)));
+        let counters: HashMap<String, Counter> = HashMap::from_iter(
+            snapshot
+                .counters
+                .into_iter()
+                .map(|v| (Snapshot::derive_metric_name(&v.name, &v.metadata), v)),
+        );
+        let gauges: HashMap<String, Gauge> = HashMap::from_iter(
+            snapshot
+                .gauges
+                .into_iter()
+                .map(|v| (Snapshot::derive_metric_name(&v.name, &v.metadata), v)),
+        );
+        let histograms: HashMap<String, Histogram> = HashMap::from_iter(
+            snapshot
+                .histograms
+                .into_iter()
+                .map(|v| (Snapshot::derive_metric_name(&v.name, &v.metadata), v)),
+        );
 
         Self {
             ts,
